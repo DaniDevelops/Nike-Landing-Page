f3f17095149e14a4263b2870590b6536ace8a1a4
Daniel Nwafor <danidev3lops@gmail.com>
2024-08-01 22:09:15 +0000

    Implement Nike Landing Page

---
+++ src/Section Components/index.js
@@ -0,0 +1,21 @@
+import CustomerReviews from './CustomerReviews'
+import Footer from  './Footer'
+import Hero from './Hero'
+import Nav from '../Components/Nav'
+import SuperQuality from './SuperQuality'
+import SpecialOffer from './SpecialOffer'
+import PopularProducts from './PopularProducts'
+import Services from './Services'
+import Subscribe from './Subscribe'
+
+export {
+    Hero, 
+    CustomerReviews,
+    Footer,
+    Nav,
+    SuperQuality,
+    SpecialOffer,
+    Services,
+    Subscribe,
+    PopularProducts
+}
